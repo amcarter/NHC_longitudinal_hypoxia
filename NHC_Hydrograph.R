7cb78a43791177d68ba603ddb7d6b32c4c9d5ab7
Alice Carter <alicecarter05@gmail.com>
2020-10-26 22:36:49 -0400

    made annual hydrograph and temperature plots for NHC

--- NHC_Hydrograph.R
+++ NHC_Hydrograph.R
@@ -1,5 +1,7 @@
 ### Plot average annual hydrograph for USGS Blands Station 
 # 2020-02-19
+### Plot average annual hydrograph for SP NHC station 
+# 2020-10-26
 
 # Updates:
 #       2020-03-27 Added section to calculate NHC baseflow and compare to wwtp discharge
@@ -8,11 +10,77 @@ library(zoo)
 library(ggplot2)
 #install.packages("RHydro", repos="http://R-Forge.R-project.org")
 library(RHydro)
-
+library(StreamPULSE)
+setwd(hypox_projdir)
 dat<- read.csv("data/raw/NHC_Blands_annualdata.csv", header=T)
+dat <- read_csv("data/NHC_Q_and_temp.csv")
+dat$date <- as.Date(dat$DateTime_UTC, tz = "EST")
+dat <- dat %>% group_by(date) %>%
+        summarise(discharge_m3s = mean(discharge_m3s, na.rm = T),
+                  temp_C = mean(water_temp_C, na.rm = T)) 
+dat$doy <- format(dat$date, "%j")
+dat$year <- year(dat$date)
+nhc2018 <- dat %>%
+        filter(year == 2018)
 
+nhc_sum <- dat %>%
+        group_by(doy) %>%
+        summarise(q.25 = quantile(discharge_m3s, 0.025, na.rm = T),
+                  q.75 = quantile(discharge_m3s, 0.975, na.rm = T),
+                  t.25 = quantile(temp_C, 0.025, na.rm = T),
+                  t.75 = quantile(temp_C, 0.975, na.rm = T))
 
-dat$doy <- seq(1,366)
+nhc_day <- nhc2018 %>% filter(date == as.Date("2018-05-25"))
+nhc_sens <- nhc2018 %>% 
+        filter(date >= as.Date("2018-06-12"), date <= as.Date("2018-07-06"))
+summary(dat)
+png("figures/NHC_average_Q_and_t.png", width = 7, height = 5, 
+    res = 300, units = "in")
+        par(mfrow = c(2,1), oma = c(0,0,2,0), mar = c(0,4,1,2))                  
+        plot(nhc_sum$doy, nhc_sum$q.75, log = 'y', type = "n",
+             ylab = "discharge (m3/s)", ylim = c(.0005,200), 
+             xaxt = "n", xlab = "") 
+        polygon(x = na.approx(c(nhc_sum$doy, rev(nhc_sum$doy))),
+                y = na.approx(c(nhc_sum$q.25, rev(nhc_sum$q.75))),
+                col = "grey80", border = NA)
+        lines(nhc2018$doy, nhc2018$discharge_m3s, lwd = 2)
+        lines(nhc_sens$doy, nhc_sens$discharge_m3s, lwd = 2, col = "brown3")
+        
+        points(nhc_day$doy, nhc_day$discharge_m3s, pch = 19, 
+               col = "brown3", cex = 1.3)
+        
+        par(mar = c(4,4,0,2))
+        plot(nhc_sum$doy, nhc_sum$t.75, type = "n",
+             ylab = "water temp (C)", ylim = c(0,28), xlab = "day of year") 
+        polygon(x = na.approx(c(nhc_sum$doy, rev(nhc_sum$doy))),
+                y = na.approx(c(nhc_sum$t.25, rev(nhc_sum$t.75))),
+                col = "grey80", border = NA)
+        lines(nhc2018$doy, nhc2018$temp_C, lwd = 2)
+        lines(nhc_sens$doy, nhc_sens$temp_C, lwd = 2, col = "brown3")
+        points(nhc_day$doy, nhc_day$temp_C, pch = 19, col = "brown3", cex = 1.3)
+        
+        
+        par(new = T, oma = c(0,0,0,0), mfrow = c(1,1))
+        plot(1,1, type = "n", xaxt = "n", yaxt = "n",
+             xlab = "", ylab = "", bty = "n" )
+        legend("top", ncol = 2, bty = "n", cex = .9,
+               legend = c("Longitudinal sampling","sensor deployment", 
+                          "   2018 data", "2016-2019 IQ range"),
+               lty = c(NA, 1, 1, NA),
+               pch = c(19, NA, NA, NA),
+               col = c("brown3", "brown3", "black", NA),
+               fill = c(NA, NA, NA, "grey80"), 
+               border = NA )
+
+dev.off()
+
+dat %>%
+        mutate(month = month(date)) %>%
+        filter(month %in% c(5,6)) %>%
+        select(discharge_m3s, temp_C) %>%
+        summary()
+        # summarize(across(everything(), mean, na.rm = TRUE))
+        # summarize(across(everything(), ~mean(., na.rm = TRUE)))
 
 dat <- select(dat, doy, mean_va, p05_va, p95_va, mean_2018) 
 dat[,2:5] <- dat[,2:5]*(12*2.54/100)^3  # convert from ft3 to m3
